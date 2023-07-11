7b6a45a7be603d5446fbf8957475eef11df9a02c
Sai Kumar Kanuri <saikumar.kanuri.ksvs@gmail.com>
2023-07-11 22:01:17 +0530

    final commit

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,7 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const generateToken = require("../utils/generateToken");
-const { use } = require("../routes/userRoutes");
 
 
 
@@ -29,7 +28,7 @@ const registerUser = asyncHandler(async (req, res) => {
             email: user.email,
             isAdmin: user.isAdmin,
             pic: user.pic,
-            token:generateToken(user._id),
+            token: generateToken(user._id),
         })
     } else {
         res.status(400);
@@ -50,7 +49,7 @@ const authUser = asyncHandler(async (req, res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token:generateToken(user._id),
+            token: generateToken(user._id),
             pic: user.pic,
         })
     } else {
@@ -60,4 +59,32 @@ const authUser = asyncHandler(async (req, res) => {
 
 });
 
-module.exports = { registerUser, authUser };
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        user.pic = req.body.pic || user.pic;
+
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            pic: updatedUser.pic,
+            token: generateToken(updatedUser._id),
+        });
+    } else {
+        res.status(404);
+        throw new Error("User Not Found");
+    }
+
+});
+
+module.exports = { registerUser, authUser, updateUserProfile };
